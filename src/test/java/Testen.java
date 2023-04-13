f1e09d08c88f30afe88e5802313587e24b0ce966
Niels van der Hoeven <nielsvanderhoeven082@gmail.com>
2023-04-13 15:08:26 +0200

    Dikke shit ouwe
    Fucking dik

--- src/test/java/Testen.java
+++ src/test/java/Testen.java
@@ -1,4 +1,7 @@
+import org.example.EchoClient;
 import org.example.GreetClient;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -6,11 +9,36 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Testen {
+    EchoClient client;
+    @Before
+    public void setup() throws IOException {
+        client = new EchoClient();
+        client.startConnection("127.0.0.1", 4444);
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        client.stopConnection();
+    }
+
+//    @Test
+//    public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() throws IOException {
+//        GreetClient client = new GreetClient();
+//        client.startConnection("127.0.0.1", 6666);
+//        String response = client.sendMessage("hello server");
+//        assertEquals("hello client", response);
+//    }
+
     @Test
-    public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() throws IOException {
-        GreetClient client = new GreetClient();
-        client.startConnection("127.0.0.1", 6666);
-        String response = client.sendMessage("hello server");
-        assertEquals("hello client", response);
+    public void givenClient_whenServerEchosMessage_thenCorrect() throws IOException {
+        String resp1 = client.sendMessage("hello");
+        String resp2 = client.sendMessage("world");
+        String resp3 = client.sendMessage("!");
+        String resp4 = client.sendMessage(".");
+
+        assertEquals("hello", resp1);
+        assertEquals("world", resp2);
+        assertEquals("!", resp3);
+        assertEquals("good bye", resp4);
     }
 }
